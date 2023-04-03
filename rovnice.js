d25631356e3283ab4a13643d9d21a2dca0605f70
Ivushfff <filipovai@spsstav.cz>
2023-04-03 11:36:21 +0200

    výpočet

--- rovnice.js
+++ rovnice.js
@@ -1,2 +1,12 @@
+function pocitej() {
+    let t1 = document.getElementById("t1").value;
+    let t2 = document.getElementById("t2").value;
+    let t = document.getElementById("t").value;
+    let m2 = document.getElementById("m2").value;
+    let c = document.getElementById("c").value;
 
-let objem = document.getElementById("objem").value;
+    let sVysledek = (m2*c*(t2-t))/(c*(t-t1))
+    console.log(sVysledek)
+
+    document.getElementById("vysledek").innerText = sVysledek
+}
